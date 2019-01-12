e7e46442a2db91b9b598aebf08588e5af3f6fd2c
knazarenko <nko@ukr.net>
2019-01-12 19:50:02 +0200

    Updates

---
+++ precache-manifest.d31d8e5c069352bf0f6c8a58b4247758.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "09b8ec9decf5e09231ef",
+    "url": "/todo/static/js/main.09b8ec9d.chunk.js"
+  },
+  {
+    "revision": "5de200cbde5ca2b1c7ae",
+    "url": "/todo/static/js/1.5de200cb.chunk.js"
+  },
+  {
+    "revision": "09b8ec9decf5e09231ef",
+    "url": "/todo/static/css/main.5dfa5596.chunk.css"
+  },
+  {
+    "revision": "5de200cbde5ca2b1c7ae",
+    "url": "/todo/static/css/1.6841c499.chunk.css"
+  },
+  {
+    "revision": "bf53dd835f9ac23486e1c3724c7911fa",
+    "url": "/todo/index.html"
+  }
+];
